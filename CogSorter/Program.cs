11429200398bc3270e960394e10d9719725f1853
Huang Jie <root@linuxsand.info>
2017-05-23 17:39:40 +0800

    first commit

---
+++ CogSorter/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Cognex.VisionPro;
+using Cognex.VisionPro.QuickBuild;
+
+namespace CogSorter
+{
+    class Program
+    {
+        static CogJobManager manager = null;
+        static void Main()
+        {
+            const string HELP = @"https://github.com/linuxsand/CogSorter/
+USAGE:
+> load xxx.vpp
+> list
+> sort 1,2,0
+> save yyy.vpp
+";
+            bool hasLoaded = false;
+            Console.WriteLine(HELP);
+            while (true)
+            {
+                Console.Write("\r\n> ");
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) return;
+
+                var parts = line.Split(new char[] {' '});
+                string action = parts[0].ToLower();
+                if (action == "load")
+                {
+                    if (parts.Length != 2)
+                    {
+                        Console.WriteLine(HELP);
+                        break;
+                    }
+                    hasLoaded = loadManager(parts[1]);
+                }
+                else if (action == "list" && hasLoaded)
+                {
+                    listJobs();
+                }
+                else if (action == "sort")
+                {
+                    if (parts.Length != 2)
+                    {
+                        Console.WriteLine(HELP);
+                        break;
+                    }
+                    sort(parts[1]);
+                }
+                else if (action == "save")
+                {
+                    saveToFile(parts[1]);
+                }
+                else if (action == "exit")
+                {
+                    break;
+                }
+            }
+            unload();
+            Console.WriteLine("exited");
+        }
+
+        static void unload()
+        {
+            if (manager != null)
+            {
+                manager.Shutdown();
+            }
+        }
+
+        static void saveToFile(string vppPath)
+        {
+            try
+            {
+                CogSerializer.SaveObjectToFile(manager, vppPath);
+                Console.WriteLine("saved");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("sorry, can not save");
+            }
+        }
+
+        static bool loadManager(string vppPath)
+        {
+            if (manager != null)
+            {
+                unload();
+            }
+
+            if (!File.Exists(vppPath))
+            {
+                Console.WriteLine("file does not exist");
+                return false;
+            }
+            Console.WriteLine("loading...");
+            try
+            {
+                manager = (CogJobManager)CogSerializer.LoadObjectFromFile(vppPath);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("can not load");
+                return false;
+            }
+            Console.WriteLine("loaded");
+            listJobs();
+            return true;
+
+        }
+
+        static void listJobs()
+        {
+            if (manager != null)
+            {
+                for (int i = 0; i < manager.JobCount; i++)
+                {
+                    Console.WriteLine(string.Format("{0}:\t\t{1}", manager.Job(i).Name, i)); 
+                }
+            }
+        }
+
+        static void sort(string seqStr)
+        {
+            if (manager == null) return;
+            // 1,2,0
+            var seq = seqStr.Split(new char[] { ',' });
+            /*
+             * job_a, 0 --> 1
+             * job_b, 1 --> 2
+             * job_c, 2 --> 0
+             * 
+             * 1, 2, 0
+             * 
+             * job_c, job_b, job_a
+            */
+            HashSet<string> sets = new HashSet<string>(seq);
+            if (sets.Count != manager.JobCount)
+            {
+                Console.WriteLine("user input numbers have duplicated value");
+                return;
+            }
+            int jobCount = manager.JobCount;
+            foreach (string index in seq)
+            {
+                int _index = 0;
+                if (!int.TryParse(index, out _index) || _index < 0 || _index >= jobCount)
+                {
+                    Console.WriteLine("user input numbers are invalid");
+                    return;
+                }
+            }
+
+            List<CogJob> jobs = new List<CogJob>();
+            List<string> jobNames = new List<string>();
+            for (int i = 0; i < jobCount; i++)
+            {
+                var job = manager.Job(Convert.ToInt32(seq[i])); // 1, 2, 0 --> b, c, a
+                jobs.Add(job);
+                jobNames.Add(job.Name);
+            }
+            foreach (var name in jobNames)
+            {
+                manager.JobRemove(name);
+            }
+            foreach (var item in jobs)
+            {
+                manager.JobAdd(item);
+            }
+            Console.WriteLine("sorted:");
+            listJobs();
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,16 @@
+CogSorter is a utility that can sort CogJobs. I was confused that Cognex QuickBuild does not offer this function, that's why I made this.
+
+Simpley run CogSorter.exe with no arguments, it will print usage message, then load \*.vpp file, input sequence that you expect, save to new file path.
+
+    C:\> CogSorter.exe
+    USAGE:
+    > load xxx.vpp
+    > list
+    > sort 1,2,0
+    > save yyy.vpp
+    
+    > 
+
+-----
+
+Public Domain
